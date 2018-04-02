50aa77693013b77d81f0a248864bc2e4c719a9e6
Zhou Kang <dev.zhoukang@gmail.com>
2018-04-02 11:31:07 +0800

    sync

---
+++ ZKScrapeDemo/ZKScrapeDemo/Scratch/ZKScratchImageView.h
@@ -0,0 +1,24 @@
+//
+//  ZKScratchImageView.h
+//  ZKScrapeDemo
+//
+//  Created by Zhou Kang on 2018/4/2.
+//  Copyright © 2018年 Zhou Kang Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class ZKScratchImageView;
+
+@protocol ZKScratchImageViewDelegate <NSObject>
+@required
+- (void)scratchImageView:(ZKScratchImageView *)scratchImageView didChangeMaskingProgress:(CGFloat)maskingProgress;
+@end
+
+@interface ZKScratchImageView : UIImageView
+
+@property (nonatomic, readonly) CGFloat                         maskingProgress;
+@property (nonatomic, weak) id <ZKScratchImageViewDelegate>     delegate;
+
+- (void)setImage:(UIImage *)image radius:(size_t)radius;
+
+@end
